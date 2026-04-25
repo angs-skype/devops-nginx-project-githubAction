7a5f96b1f3548bfdc7f020bba200f8702e26d449
angs-skype <144552488+angs-skype@users.noreply.github.com>
2026-04-25 16:53:54 +0000

    setting 4

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
 resource "aws_key_pair" "deployer" {
@@ -34,7 +34,7 @@ resource "aws_security_group" "nginx_sg" {
 
 resource "aws_instance" "nginx_ec2" {
   ami           = "ami-0f58b397bc5c1f2e8" # Amazon Linux 2 (update if needed)
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name      = aws_key_pair.deployer.key_name
 
   vpc_security_group_ids = [aws_security_group.nginx_sg.id]
